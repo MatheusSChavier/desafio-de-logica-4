dd6982c5cf62a56f868052dcada0e3c5cf5b2184
kenykenoei <matheuschavier112004@gmail.com>
2021-06-04 15:25:31 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,20 @@
+function X(X){
+    console.log(X)
+}
+X("X")
+function myName(name){
+    console.log("meu nome é "+name)
+}
+myName("Matheus")
+function I(name1,age,favoritMusic){
+    console.log("meu nome é "+name1+" tenho "+age+" anos, e minha musica favorita é "+favoritMusic)
+}
+I("Matheus",17,"Me diga então")
+function II(favoritMovie,favoritMusic1){
+    console.log("meu filem vavorito é "+favoritMovie+" e minha musica é "+favoritMusic1)
+}
+II("Star Wars VI","Me diga então")
+function III(number1,number2){
+    console.log(number1*number2)
+}
+III(9,10)
